b4706ae73e36fa7bbf7fdf70217661961c1f130d
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-11-25 20:19:25 +0900

    Maxheap에서 내려오는 함수 구현

--- Maxheap.cpp
+++ Maxheap.cpp
@@ -9,3 +9,17 @@ void printHeap(vector<int>& heap) {
     cout << endl;
 }
 
+void buildMaxHeapTopDown(vector<int>& heap) {
+    for (int i = 2; i < heap.size(); ++i) {
+        int current = i;
+        while (current > 1 && heap[current] > heap[current / 2])
+        {
+            swap(heap[current], heap[current / 2]);
+            current /= 2;
+        }
+        
+    }
+}
+
+
+
